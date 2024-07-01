8960a79ce14531fa4de059dfb20a8183b738ddfb
Daniel Madalitso Phiri <malgamves@gmail.com>
2024-07-01 15:23:42 +0200

    taken main pages out

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,13 @@
 import React from 'react';
-import Image from "next/image";
-
-import SearchResult from "../components/SearchResult.tsx";
-import Header from "../components/Header.tsx";
-
 
 export default async function Home() {
 
   return (
     <div>
       <main>
-        <div className="items-center justify-center flex pt-12">
-          <Image
-            className="w-48"
-            src="/logo.png"
-            alt="Weaviate Logo"
-            height={360}
-            width={360}
-          />
-        </div>
-        <Header text={"Vector Search with Weaviate"}/>
-        <SearchResult />
+        <h1>
+          Hello
+        </h1>
       </main>
     </div>
   );
